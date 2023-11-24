a1fca370122bdde3c0a8220aaa57e0d39008ff62
Beaprofessor <professornberlin2122@gmail.com>
2023-11-24 22:33:58 +0530

    Introduction to JS

--- Day54/script.js
+++ Day54/script.js
@@ -1,3 +1,25 @@
 alert("Hello Guys");
  console.log("Code is running.....")
  console.log("Lookin like a wowwwww.....")
+
+
+ var a  = prompt("Enter your number... ")
+ var isTrue = confirm("Are you sure u want to burn your computer...") 
+
+
+ if(isTrue){
+     console.log("The computer is blasting")
+ }
+
+ else{
+     console.log("Your computer is not  blasting.")
+ }
+
+ console.log("Enter your number " +  a)
+console.log("")
+
+
+
+
+
+
