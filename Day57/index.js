2565621eeb7196c79bbc3ab1dce1978d2ea70897
Beaprofessor <professornberlin2122@gmail.com>
2023-11-28 11:16:10 +0530

    JavaScript Loops

---
+++ Day57/index.js
@@ -0,0 +1,45 @@
+console.log("I am on the tutorial on LOOPS");
+
+
+// let a = 1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+
+// for (let i = 1; i <=100; i++) {
+
+//     console.log(i);
+    
+// }
+
+// let obj = {
+//     name: "Harry",
+//     role: "Programmer",
+//     company:"Ai pvt ltd", 
+
+// }
+
+// for (const key in obj) {
+//     {
+//         console.log(key) ;   
+//     }
+// }
+
+// for (const c of "Harry") {
+//     console.log(c);
+// }
+
+
+// let i = 0;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i<0);
